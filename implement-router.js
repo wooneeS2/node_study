73fc220273e6436c69169346a08440582af6d667
woonees2 <soyoung0007@naver.com>
2023-12-04 21:44:56 +0900

    라우터 만들어보자 - 리팩토링

--- implement-router.js
+++ implement-router.js
@@ -1,19 +1,35 @@
 const http = require("http");
 
+//url 모듈을 로딩하고 url qustndp gkfekd
 const url = require("url");
 
 http
   .createServer((req, res) => {
+    //url 모듈을 사용해 요청으로 받은 url의 Pathname 얻기
+    //true는 쿼리 스트링도 함께 파싱할지 여부 설정
     const path = url.parse(req.url, true).pathname;
+
     res.setHeader("Content-Type", "text/html");
 
     if (path === "/user") {
-      res.end("[user] name : andy, age:30");
+      user(req, res);
     } else if (path === "/feed") {
-      res.end(`<ul> <li>picture</li><li>hello</li></ul>`);
+      feed(req, res);
     } else {
       res.statusCode = 404;
-      res.end("404 page not found");
+      notFound(req, res);
     }
   })
   .listen("3000", () => console.log("Make router!"));
+
+const user = (req, res) => {
+  res.end("[user] name : andy, age:30");
+};
+
+const feed = (req, res) => {
+  res.end(`<ul> <li>picture</li><li>hello</li></ul>`);
+};
+
+const notFound = (req, res) => {
+  res.end("404 page not found");
+};
